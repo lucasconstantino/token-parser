a684edffbff94c0ae7c010965e7666a323b93c14
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-02-25 20:15:40 -0300

    Created filters api.

--- index.js
+++ index.js
@@ -2,26 +2,44 @@
  * Token Parser
  */
 
-var expressionParser = require('angular-expressions')
+var ExpressionParser = require('./lib/expression-parser')
   , Lexer = require('./lib/lexer')
   , types = require('./lib/node-types')
   , CompoundType = types['COMPOUND'];
 
-function TokenParser() {
+function TokenParser(cache) {
   this.lexer = new Lexer();
   this.errorHandlers = [];
   this.nodeTypes = [];
+  this.expressions = new ExpressionParser(cache);
 };
 
 TokenParser.prototype.init = function () {
   this.lexer.init();
 
   // Register core node types.
-  for (var i in types) types.hasOwnProperty(i) && this.addNodeType(i, types[i]);
+  for (var i in types) types.hasOwnProperty(i) && this.nodeType(i, types[i]);
 };
 
-TokenParser.prototype.addNodeType = function (name, compiler) {
-  this.nodeTypes[name] = compiler;
+TokenParser.prototype.nodeType = function (name, compiler) {
+  return typeof compiler === 'undefined' ? this.nodeTypes[name] : (this.nodeTypes[name] = compiler);
+  if (typeof compiler === 'undefined') return this.nodeTypes[name];
+  if (typeof compiler === false) {
+    var compiler = this.nodeTypes[name];
+    delete this.nodeTypes[name];
+    return compiler;
+  }
+  return (this.nodeTypes[name] = compiler);
+};
+
+TokenParser.prototype.filter = function (name, filter) {
+  if (typeof filter === 'undefined') return this.expressions.filters[name];
+  if (typeof filter === false) {
+    var filter = this.expressions.filters[name];
+    delete this.expressions.filters[name];
+    return filter;
+  }
+  return (this.expressions.filters[name] = filter);
 };
 
 TokenParser.prototype.scan = function (text) {
@@ -57,8 +75,11 @@ TokenParser.prototype.compile = function (text) {
   return new CompoundType(nodes);
 };
 
-TokenParser.prototype.replace = function (text, context) {
-  return this.compile(text).replace(context);
+TokenParser.prototype.replace = function (text) {
+  var compound = this.compile(text)
+    , args = [].slice.call(arguments, 1);
+
+  return compound.replace.apply(compound, args);
 };
 
 TokenParser.prototype.addErrorHandler = function (callback) {
