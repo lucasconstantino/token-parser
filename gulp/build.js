d8848d4d94d0354288934cf48744d6c38bcfcaed
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-02-25 21:43:44 -0300

    Added header to distribution files.

--- gulp/build.js
+++ gulp/build.js
@@ -2,13 +2,25 @@
  * Build related tasks.
  */
 
-var gulp        = require('gulp')
+var fs          = require('fs')
+  , gulp        = require('gulp')
   , browserify  = require('browserify')
   , uglify      = require('gulp-uglify')
   , transform   = require('vinyl-transform')
   , concat      = require('gulp-concat')
   , uglify      = require('gulp-uglify')
-  , pack        = require('../package.json');
+  , header      = require('gulp-header')
+  , pkg         = require('../package.json')
+  , signature   = fs.readFileSync('./signature');
+
+/**
+ * Get signature header.
+ */
+function signatureHeader() {
+  return header(signature, {
+    pkg: pkg
+  });
+}
 
 /**
  * Unit test: uses Mocha to test TokenParser.
@@ -21,14 +33,18 @@ gulp.task('build', function () {
     return b.bundle();
   });
 
+
+
   return gulp.src(['./index.js'])
     .pipe(browserified)
-    .pipe(concat(pack.name + '.js'))
+    .pipe(concat(pkg.name + '.js'))
+    .pipe(signatureHeader())
     .pipe(gulp.dest('./dist'))
     .pipe(uglify({
       mangle: false
     }))
-    .pipe(concat(pack.name + '.min.js'))
+    .pipe(signatureHeader())
+    .pipe(concat(pkg.name + '.min.js'))
     .pipe(gulp.dest('./dist'));
 });
 
